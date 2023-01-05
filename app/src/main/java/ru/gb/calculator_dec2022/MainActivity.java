34944e09c1e1fae1ac4a0a8fde5abdbbb7e91816
YuriusR <nelf_88@mail.ru>
2023-01-05 23:29:33 +0500

    калькулятор начало

---
+++ app/src/main/java/ru/gb/calculator_dec2022/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.gb.calculator_dec2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
